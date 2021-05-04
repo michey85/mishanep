973c0c6bf409694aee91c0c4898b06c1279b9ac4
Mikhail Nepomnyashchiy <michey85@gmail.com>
2021-05-04 21:31:26 +0300

    may 2021 coupons

--- src/data/index.js
+++ src/data/index.js
@@ -14,7 +14,7 @@ export const courses = [
             'React 17 от азов до создания полноценного SPA-приложения - с роутингом и продвинутым управлением состоянием.',
         tags: ['React', 'JavaScript', 'SPA'],
         link:
-            'https://www.udemy.com/course/react-from-scratch/?couponCode=9E62D890B49968E622B2',
+            'https://www.udemy.com/course/react-from-scratch/?couponCode=22503C8C3A6EDE4385BF',
         poster: ReactImg,
     },
     {
@@ -25,7 +25,7 @@ export const courses = [
             'Освойте множество крутых инструментов в одном курсе! Контроль версий, сборка и многое другое.',
         tags: ['NPM', 'Terminal', 'Git'],
         link:
-            'https://www.udemy.com/course/webtools/?couponCode=9971F80ECF79B6936A7D',
+            'https://www.udemy.com/course/webtools/?couponCode=AE7D2FE71BED7B1C3F76',
         poster: WebToolsImg,
     },
     {
@@ -36,7 +36,7 @@ export const courses = [
             'Освойте необходимые технологии современной верстки сайтов и начните работать в сфере веб-разработки!',
         tags: ['HTML', 'CSS', 'JavaScript'],
         link:
-            'https://www.udemy.com/course/web-coder/?couponCode=2D7000A7C41D134D63A3',
+            'https://www.udemy.com/course/web-coder/?couponCode=2690560629CE716639A9',
         poster: WebCoderImg,
     },
     {
@@ -47,7 +47,7 @@ export const courses = [
             'Получите необходимые навыки для создания собственной обучающей платформы с готовой системой Moodle.',
         tags: ['LMS', 'CMS', 'Moodle'],
         link:
-            'https://www.udemy.com/course/moodle3-admin/?couponCode=40F132E66CB97BF3E7AE',
+            'https://www.udemy.com/course/moodle3-admin/?couponCode=AA70B454571A21C8A45C',
         poster: MoodleImg,
     },
     {
@@ -58,7 +58,7 @@ export const courses = [
             'Прокачайте свои навыки HTML-верстки, освоив современный инструмент веб-разработки - шаблонизатор/препроцессор Pug.',
         tags: ['Pug', 'HTML', 'JavaScript'],
         link:
-            'https://www.udemy.com/course/pugjade-html/?couponCode=B84B40B63FF9B445A0AE',
+            'https://www.udemy.com/course/pugjade-html/?couponCode=55DB1EC63252190A6331',
         poster: PugImg,
     },
     {
@@ -69,7 +69,7 @@ export const courses = [
             'Сделайте сайт себе - визитку, портфолио или небольшой магазин или получите профессию фринсера.',
         tags: ['CMS', 'WordPress'],
         link:
-            'https://www.udemy.com/course/wordpress-pcgramota/?couponCode=A6C7A0E1E04697212D7C',
+            'https://www.udemy.com/course/pugjade-html/?couponCode=55DB1EC63252190A6331',
         poster: WPImg,
     },
 ];
