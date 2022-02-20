73f074d90efdddcf5b9f7f5122cf8a2e00edd61b
Mikhail Nepomnyashchiy <nem@kodix.ru>
2022-02-20 13:29:23 +0100

    redux course was added

--- src/data/index.js
+++ src/data/index.js
@@ -4,7 +4,8 @@ import WebToolsImg from '../assets/webtools-cover-v1.png';
 import WebCoderImg from '../assets/webcoder-course-cover.jpg';
 // import MoodleImg from '../assets/moodle-course-cover.jpg';
 import PugImg from '../assets/pug-course-cover.jpg';
-import WPImg from '../assets/wordpress-course-cover.jpg';
+// import WPImg from '../assets/wordpress-course-cover.jpg';
+import RRImg from '../assets/redux-course.jpg';
 
 export const courses = [
     {
@@ -26,10 +27,19 @@ export const courses = [
         poster: ReactImg,
     },
     {
+        id: 4,
+        title: 'Redux для управления состоянием',
+        subtitle: '+2 проекта на React+Redux',
+        description: 'Освойте принципы работы Redux - на классическом и современном вариантах использования.',
+        tags: ['Redux', 'React', 'JavaScript'],
+        link: 'https://www.udemy.com/course/redux-react/?couponCode=5A8B7A4E5844377421DD',
+        poster: RRImg,
+    },
+    {
         id: 2,
         title: 'Инструменты разработчика',
         subtitle: 'Git, SSH, Webpack и другие',
-        description: 'Освойте множество крутых инструментов в одном курсе! Контроль версий, сборка и многое другое.',
+        description: 'Освойте множество крутых инструментов в одном курсе! Контроль версий, сборка, терминал, лайфхаки и многое другое.',
         tags: ['NPM', 'Terminal', 'Git'],
         link: 'https://www.udemy.com/course/webtools/?couponCode=C763B2CF06C2381BCFE2',
         poster: WebToolsImg,
@@ -63,15 +73,15 @@ export const courses = [
         link: 'https://www.udemy.com/course/pugjade-html/?couponCode=67D127F8ABCD86487C93',
         poster: PugImg,
     },
-    {
-        id: 6,
-        title: 'Создание сайтов без программирования',
-        subtitle: 'Сайты на WordPress - от простых до сложных',
-        description: 'Сделайте сайт себе - визитку, портфолио или небольшой магазин или получите профессию фринсера.',
-        tags: ['CMS', 'WordPress'],
-        link: 'https://www.udemy.com/course/wordpress-pcgramota/?couponCode=B5FB521D0D1B1D02AC39',
-        poster: WPImg,
-    },
+    // {
+    //     id: 6,
+    //     title: 'Создание сайтов без программирования',
+    //     subtitle: 'Сайты на WordPress - от простых до сложных',
+    //     description: 'Сделайте сайт себе - визитку, портфолио или небольшой магазин или получите профессию фринсера.',
+    //     tags: ['CMS', 'WordPress'],
+    //     link: 'https://www.udemy.com/course/wordpress-pcgramota/?couponCode=B5FB521D0D1B1D02AC39',
+    //     poster: WPImg,
+    // },
 ];
 
 export const bio = `Действующий веб-разработчик и опытный преподаватель. Веду направления по созданию сайтов: от самых основ до профессий веб-мастера, верстальщика и frontend-разработчика.
@@ -87,16 +97,58 @@ export const youtubeList = [
     },
     {
         id: 2,
+        title: 'Pro React',
+        link:
+            'https://www.youtube.com/watch?v=-CdrHAImYfk&list=PLiZoB8JBsdzkClMnvcedplvSF_JPhvopM',
+        poster: 'https://i.ytimg.com/vi/-CdrHAImYfk/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBU1AGKVmC3_Im7Kc3MrZ3N5qAUlA',
+    },
+    {
+        id: 3,
         title: 'Основы TypeScript',
         link:
             'https://www.youtube.com/playlist?list=PLiZoB8JBsdzlG1oAY8U4vrBtVW07j6jil',
         poster: 'https://i.ytimg.com/vi/G7fNaZwIoiM/hqdefault.jpg',
     },
     {
-        id: 3,
+        id: 4,
+        title: 'React + Redux',
+        link:
+            'https://www.youtube.com/playlist?list=PLiZoB8JBsdznQv3kAEvTzDP2qjaUI8Vo7',
+        poster: 'https://i.ytimg.com/vi/C0fBnil_Im4/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDLfEnXpIWKO6UO5W2mnwrE3xmISw',
+    },
+    {
+        id: 5,
         title: 'CSS Grid',
         link:
             'https://www.youtube.com/playlist?list=PLiZoB8JBsdzk7yebGLJSgZiGXty6YDPBD',
         poster: 'https://i.ytimg.com/vi/01SzD20gJUU/hqdefault.jpg',
+    },
+    {
+        id: 6,
+        title: 'Ramda JS',
+        link:
+            'https://www.youtube.com/playlist?list=PLiZoB8JBsdzkqsILPvz5jw2-OJ5rw6ukH',
+        poster: 'https://i.ytimg.com/vi/XkNynJBruKY/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDJAvF9H-qLX66F73RTsyFXVY3l7A',
+    },
+    {
+        id: 7,
+        title: 'React Router v6',
+        link:
+            'https://www.youtube.com/playlist?list=PLiZoB8JBsdznY1XwBcBhHL9L7S_shPGVE',
+        poster: 'https://i.ytimg.com/vi/0auS9DNTmzE/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBryXqrCbMINMKX0ZceZ8dAu6iMrg',
+    },
+    {
+        id: 8,
+        title: 'Анимации в React',
+        link:
+            'https://www.youtube.com/playlist?list=PLiZoB8JBsdzkn1yqxUaprfLYzdS-1jhJE',
+        poster: 'https://i.ytimg.com/vi/_94vYQtaz1Y/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCi4hTk2pz5eXKMuyjaxpPaprS3ig',
+    },
+    {
+        id: 9,
+        title: 'Инструменты Разработчика',
+        link:
+            'https://www.youtube.com/playlist?list=PLiZoB8JBsdzlI0lIfk2zsf3F46FUkNaKC',
+        poster: 'https://i.ytimg.com/vi/GNuPi_7df4k/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBcfICEixZubdxraSpAqZpc2VzD7A',
     },
 ];
