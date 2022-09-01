4004c14632c92cb41f91535261387b745be41a53
Mikhail Nepomnyashchiy <nem@kodix.ru>
2022-09-01 23:07:51 +0200

    september 22 coupons

--- src/components/Courses/Courses.tsx
+++ src/components/Courses/Courses.tsx
@@ -59,7 +59,7 @@ const Courses: React.FC<ICourses> = (props) => {
       </MHeading>
       <MAlert status="info" variants={alertAnimation} mb={8}>
         <AlertIcon />
-        На Stepik.org до конца лета на все курсы действует промокод - VERANO22.
+        На Stepik.org до конца осени на все курсы действует промокод - OTONO22.
       </MAlert>
       <MSimpleGrid columns={{ base: 1, md: 3 }} spacing={10}>
         {
