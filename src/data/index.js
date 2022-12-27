41093ed4346a3d5c6b2b30f93c88d5d8d718418e
Mikhail Nepomnyashchiy <nem@kodix.ru>
2022-12-27 10:11:10 +0100

    bio update

--- src/data/index.js
+++ src/data/index.js
@@ -81,91 +81,91 @@ export const courses = [
     poster: PugImg,
   },
   {
-      id: 7,
-      title: 'Moodle 3 для администраторов',
-      subtitle: 'Своя обучающая платформа',
-      description:
-          'Получите необходимые навыки для создания собственной обучающей платформы с готовой системой Moodle.',
-      tags: ['LMS', 'CMS', 'Moodle'],
-      link: 'https://www.udemy.com/course/moodle3-admin/?couponCode=B256D712A3E5BA4EB34F',
-      poster: MoodleImg,
+    id: 7,
+    title: 'Moodle 3 для администраторов',
+    subtitle: 'Своя обучающая платформа',
+    description:
+      'Получите необходимые навыки для создания собственной обучающей платформы с готовой системой Moodle.',
+    tags: ['LMS', 'CMS', 'Moodle'],
+    link: 'https://www.udemy.com/course/moodle3-admin/?couponCode=B256D712A3E5BA4EB34F',
+    poster: MoodleImg,
   },
   {
-      id: 8,
-      title: 'Создание сайтов без программирования',
-      subtitle: 'Сайты на WordPress - от простых до сложных',
-      description: 'Сделайте сайт себе - визитку, портфолио или небольшой магазин или получите профессию фринсера.',
-      tags: ['CMS', 'WordPress'],
-      link: 'https://www.udemy.com/course/wordpress-pcgramota/?couponCode=AB75A893FC7ACAFB4FA2',
-      poster: WPImg,
+    id: 8,
+    title: 'Создание сайтов без программирования',
+    subtitle: 'Сайты на WordPress - от простых до сложных',
+    description: 'Сделайте сайт себе - визитку, портфолио или небольшой магазин или получите профессию фринсера.',
+    tags: ['CMS', 'WordPress'],
+    link: 'https://www.udemy.com/course/wordpress-pcgramota/?couponCode=AB75A893FC7ACAFB4FA2',
+    poster: WPImg,
   },
 ];
 
-export const bio = `Действующий веб-разработчик и опытный преподаватель. Веду направления по созданию сайтов: от самых основ до профессий веб-мастера, верстальщика и frontend-разработчика.
-Много лет работал в НТВ-Плюс, в настоящее время участвую в разработке современных веб-сервисов для автодилеров в Kodix Automotive.`;
+export const bio = `Действующий веб-разработчик и опытный преподаватель. Обучаю созданию сайтов: от самых основ до профессий веб-мастера, верстальщика и frontend-разработчика - JavaScript, TypeScript, React, Redux и многое другое. В настоящий момент работаю в Испании в EPAM Systems.
+`;
 
 export const youtubeList = [
   {
     id: 1,
     title: 'Фундаментальный JS',
     link:
-            'https://www.youtube.com/playlist?list=PLiZoB8JBsdznAXu9WxE5NWfPwp22XWnM7',
+      'https://www.youtube.com/playlist?list=PLiZoB8JBsdznAXu9WxE5NWfPwp22XWnM7',
     poster: 'https://i.ytimg.com/vi/l7fHH0YA7vY/hqdefault.jpg',
   },
   {
     id: 2,
     title: 'Pro React',
     link:
-            'https://www.youtube.com/watch?v=-CdrHAImYfk&list=PLiZoB8JBsdzkClMnvcedplvSF_JPhvopM',
+      'https://www.youtube.com/watch?v=-CdrHAImYfk&list=PLiZoB8JBsdzkClMnvcedplvSF_JPhvopM',
     poster: 'https://i.ytimg.com/vi/-CdrHAImYfk/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBU1AGKVmC3_Im7Kc3MrZ3N5qAUlA',
   },
   {
     id: 3,
     title: 'Основы TypeScript',
     link:
-            'https://www.youtube.com/playlist?list=PLiZoB8JBsdzlG1oAY8U4vrBtVW07j6jil',
+      'https://www.youtube.com/playlist?list=PLiZoB8JBsdzlG1oAY8U4vrBtVW07j6jil',
     poster: 'https://i.ytimg.com/vi/G7fNaZwIoiM/hqdefault.jpg',
   },
   {
     id: 4,
     title: 'React + Redux',
     link:
-            'https://www.youtube.com/playlist?list=PLiZoB8JBsdznQv3kAEvTzDP2qjaUI8Vo7',
+      'https://www.youtube.com/playlist?list=PLiZoB8JBsdznQv3kAEvTzDP2qjaUI8Vo7',
     poster: 'https://i.ytimg.com/vi/C0fBnil_Im4/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDLfEnXpIWKO6UO5W2mnwrE3xmISw',
   },
   {
     id: 5,
     title: 'CSS Grid',
     link:
-            'https://www.youtube.com/playlist?list=PLiZoB8JBsdzk7yebGLJSgZiGXty6YDPBD',
+      'https://www.youtube.com/playlist?list=PLiZoB8JBsdzk7yebGLJSgZiGXty6YDPBD',
     poster: 'https://i.ytimg.com/vi/01SzD20gJUU/hqdefault.jpg',
   },
   {
     id: 6,
     title: 'Ramda JS',
     link:
-            'https://www.youtube.com/playlist?list=PLiZoB8JBsdzkqsILPvz5jw2-OJ5rw6ukH',
+      'https://www.youtube.com/playlist?list=PLiZoB8JBsdzkqsILPvz5jw2-OJ5rw6ukH',
     poster: 'https://i.ytimg.com/vi/XkNynJBruKY/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDJAvF9H-qLX66F73RTsyFXVY3l7A',
   },
   {
     id: 7,
     title: 'React Router v6',
     link:
-            'https://www.youtube.com/playlist?list=PLiZoB8JBsdznY1XwBcBhHL9L7S_shPGVE',
+      'https://www.youtube.com/playlist?list=PLiZoB8JBsdznY1XwBcBhHL9L7S_shPGVE',
     poster: 'https://i.ytimg.com/vi/0auS9DNTmzE/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBryXqrCbMINMKX0ZceZ8dAu6iMrg',
   },
   {
     id: 8,
     title: 'Анимации в React',
     link:
-            'https://www.youtube.com/playlist?list=PLiZoB8JBsdzkn1yqxUaprfLYzdS-1jhJE',
+      'https://www.youtube.com/playlist?list=PLiZoB8JBsdzkn1yqxUaprfLYzdS-1jhJE',
     poster: 'https://i.ytimg.com/vi/_94vYQtaz1Y/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCi4hTk2pz5eXKMuyjaxpPaprS3ig',
   },
   {
     id: 9,
     title: 'Инструменты Разработчика',
     link:
-            'https://www.youtube.com/playlist?list=PLiZoB8JBsdzlI0lIfk2zsf3F46FUkNaKC',
+      'https://www.youtube.com/playlist?list=PLiZoB8JBsdzlI0lIfk2zsf3F46FUkNaKC',
     poster: 'https://i.ytimg.com/vi/GNuPi_7df4k/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBcfICEixZubdxraSpAqZpc2VzD7A',
   },
 ];
