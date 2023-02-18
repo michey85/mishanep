25c32830deebfba46eb815f5912bd03611115396
Mikhail Nepomnyashchiy <nem@kodix.ru>
2023-02-18 22:45:24 +0100

    chakra-ui update to v2

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,12 @@
 import * as React from 'react';
 import { createRoot } from 'react-dom/client';
-import { ColorModeScript } from '@chakra-ui/react';
 
 import { App } from './components/App';
-
 import './index.css';
 
-const root = createRoot(document.getElementById('root') as Element);
+const container = document.getElementById('root') as Element
+const root = createRoot(container);
 
 root.render(
-  <>
-    <ColorModeScript />
     <App />
-  </>,
 );
