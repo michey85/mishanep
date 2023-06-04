af4a6c7407e201f6a9d55f8a3c62decabfc5ee19
Mikhail Nepomniashchii <michey85@gmail.com>
2023-06-04 10:31:34 +0200

    stepik summer coupon

--- src/components/Courses/Courses.tsx
+++ src/components/Courses/Courses.tsx
@@ -59,7 +59,7 @@ const Courses: React.FC<ICourses> = (props) => {
       </MHeading>
       <MAlert status="info" variants={alertAnimation} mb={8}>
         <AlertIcon />
-        На Stepik.org всю весну на все курсы действует промокод - VERANO23.
+        На Stepik.org всё лето на все курсы действует промокод - VERANO23.
       </MAlert>
       <MSimpleGrid columns={{ base: 1, md: 3 }} spacing={10}>
         {
