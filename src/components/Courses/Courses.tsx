d6b5fb7aeb3d914018ad630160bcb9e746db9bec
Mikhail Nepomniashchii <michey85@gmail.com>
2023-09-02 10:32:31 +0200

    coupons otono 23

--- src/components/Courses/Courses.tsx
+++ src/components/Courses/Courses.tsx
@@ -59,7 +59,7 @@ const Courses: React.FC<ICourses> = (props) => {
       </MHeading>
       <MAlert status="info" variants={alertAnimation} mb={8}>
         <AlertIcon />
-        На Stepik.org всё лето на все курсы действует промокод - VERANO23.
+        На Stepik.org осенью на все курсы действует промокод - OTONO23.
       </MAlert>
       <MSimpleGrid columns={{ base: 1, md: 3 }} spacing={10}>
         {
