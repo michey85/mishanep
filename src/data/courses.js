238dfb0cda92bdfc050bb66a1f0795c8eef4c28a
Mikhail Nepomniashchii <michey85@gmail.com>
2023-10-27 21:16:50 +0200

    stepik links update

--- src/data/courses.js
+++ src/data/courses.js
@@ -8,6 +8,8 @@ import TSImg from '../assets/ts.png';
 import WebCoderImg from '../assets/webcoder-course-cover.jpg';
 import WebToolsImg from '../assets/webtools-cover-v1.png';
 
+const STEPIK_UTM = '?utm_source=mishanep&utm_medium=link&utm_campaign=directlink';
+
 export const courses = [
   {
     id: 8,
@@ -16,8 +18,7 @@ export const courses = [
     description:
       'Пройдите путь от основ HTML до создания современных приложений на React и Redux.',
     tags: ['HTML', 'JavaScript', 'React'],
-    stepik:
-      'https://stepik.org/a/180789?utm_source=mishanep&utm_medium=link&utm_campaign=directlink',
+    stepik: 'https://stepik.org/a/180789' + STEPIK_UTM,
     poster: FrontImg,
   },
   {
@@ -28,8 +29,7 @@ export const courses = [
       'Современный JavaScript с основ до продвинутых техник. Для начинающих и желающих закрыть пробелы.',
     tags: ['JavaScript'],
     link: 'https://www.udemy.com/course/fundamental-javascript/?couponCode=20E322FCF50AD045AA5D',
-    stepik:
-      'https://stepik.org/a/114165?utm_source=mishanep&utm_medium=link&utm_campaign=directlink',
+    stepik: 'https://stepik.org/a/114165' + STEPIK_UTM,
     poster: JSImg,
   },
   {
@@ -40,8 +40,7 @@ export const courses = [
       'React 17 от азов до создания полноценного SPA-приложения - с роутингом и продвинутым управлением состоянием.',
     tags: ['React', 'JavaScript', 'SPA'],
     link: 'https://www.udemy.com/course/react-from-scratch/?couponCode=03291ACF707DC2689CA9',
-    stepik:
-      'https://stepik.org/z/114197?utm_source=mishanep&utm_medium=link&utm_campaign=directlink',
+    stepik: 'https://stepik.org/a/114197' + STEPIK_UTM,
     poster: ReactImg,
   },
   {
@@ -52,8 +51,7 @@ export const courses = [
       'Освойте принципы работы Redux - на классическом и современном вариантах использования.',
     tags: ['Redux', 'React', 'JavaScript'],
     link: 'https://www.udemy.com/course/redux-react/?couponCode=474831C17FF7CB90E876',
-    stepik:
-      'https://stepik.org/z/113714?utm_source=mishanep&utm_medium=link&utm_campaign=directlink',
+    stepik: 'https://stepik.org/a/113714' + STEPIK_UTM,
     poster: RRImg,
   },
   {
@@ -64,8 +62,7 @@ export const courses = [
       'От базовых типов до создания серьезных приложений на современном стеке.',
     tags: ['TypeScript', 'Redux', 'React'],
     link: 'https://www.udemy.com/course/modern-typescript/?couponCode=F3A5F47F7EA74DE3CFB3',
-    stepik:
-      'https://stepik.org/a/121859?utm_source=mishanep&utm_medium=link&utm_campaign=directlink',
+    stepik: 'https://stepik.org/a/121859' + STEPIK_UTM,
     poster: TSImg,
   },
   {
@@ -76,8 +73,7 @@ export const courses = [
       'Освойте множество крутых инструментов в одном курсе! Контроль версий, сборка, терминал, лайфхаки и многое другое.',
     tags: ['NPM', 'Terminal', 'Git'],
     link: 'https://www.udemy.com/course/webtools/?couponCode=C0633EDC4E2EA6EDED90',
-    stepik:
-      'https://stepik.org/z/114174?utm_source=mishanep&utm_medium=link&utm_campaign=directlink',
+    stepik: 'https://stepik.org/a/114174' + STEPIK_UTM,
     poster: WebToolsImg,
   },
   {
@@ -88,8 +84,7 @@ export const courses = [
       'Освойте минимально необходимые технологии современной верстки сайтов и начните работать веб-разработчиком в IT сфере!',
     tags: ['HTML', 'CSS', 'JavaScript'],
     link: 'https://www.udemy.com/course/web-coder/?couponCode=F11C9E9F70A9D3DA9613',
-    stepik:
-      'https://stepik.org/z/120081?utm_source=mishanep&utm_medium=link&utm_campaign=directlink',
+    stepik: 'https://stepik.org/a/120081' + STEPIK_UTM,
     poster: WebCoderImg,
   },
   {
@@ -100,8 +95,7 @@ export const courses = [
       'Прокачайте свои навыки HTML-верстки, освоив современный инструмент веб-разработки - шаблонизатор/препроцессор Pug.',
     tags: ['Pug', 'HTML', 'JavaScript'],
     link: 'https://www.udemy.com/course/pugjade-html/?couponCode=61E720E3A9CB89A05483',
-    stepik:
-      'https://stepik.org/z/120077?utm_source=mishanep&utm_medium=link&utm_campaign=directlink',
+    stepik: 'https://stepik.org/a/120077' + STEPIK_UTM,
     poster: PugImg,
   },
   {
