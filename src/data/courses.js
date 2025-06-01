0692846148d02028f70b39f5dc3e1528c9540675
Mikhail Nepomniashchii <michey85@gmail.com>
2025-06-01 12:12:14 +0200

    udemy june 25

--- src/data/courses.js
+++ src/data/courses.js
@@ -49,7 +49,7 @@ export const courses = [
     description:
       'Современный JavaScript с основ до продвинутых техник. Для начинающих и желающих закрыть пробелы.',
     tags: ['JavaScript'],
-    link: 'https://www.udemy.com/course/fundamental-javascript/?couponCode=D398013FC8C9EBEF0731',
+    link: 'https://www.udemy.com/course/fundamental-javascript/?couponCode=8BC517310C0C7A6CC99C',
     stepik: 'https://stepik.org/a/114165/pay?promo=87d7a939552c931c' + STEPIK_UTM,
     poster: JSImg,
   },
@@ -60,7 +60,7 @@ export const courses = [
     description:
       'React 17 от азов до создания полноценного SPA-приложения - с роутингом и продвинутым управлением состоянием.',
     tags: ['React', 'JavaScript', 'SPA'],
-    link: 'https://www.udemy.com/course/react-from-scratch/?couponCode=242DCBD8E123252C6CA0',
+    link: 'https://www.udemy.com/course/react-from-scratch/?couponCode=09EEBB256BA7BE23A830',
     stepik: 'https://stepik.org/a/114197/pay?promo=fd5923345f575e90' + STEPIK_UTM,
     poster: ReactImg,
   },
@@ -71,18 +71,18 @@ export const courses = [
     description:
       'Освойте принципы работы Redux - на классическом и современном вариантах использования.',
     tags: ['Redux', 'React', 'JavaScript'],
-    link: 'https://www.udemy.com/course/redux-react/?couponCode=563605675C87C1B18001',
+    link: 'https://www.udemy.com/course/redux-react/?couponCode=75C584497ABCD88FE616',
     stepik: 'https://stepik.org/a/113714/pay?promo=00402710b0a28182' + STEPIK_UTM,
     poster: RRImg,
   },
   {
     id: 3,
-    title: 'TypeScript для разработки',
+    title: 'TypeScript для профессионалов',
     subtitle: 'Добавьте строгости коду',
     description:
       'От базовых типов до создания серьезных приложений на современном стеке.',
     tags: ['TypeScript', 'Redux', 'React'],
-    link: 'https://www.udemy.com/course/modern-typescript/?couponCode=A7B92DBFBD7D735E7D90',
+    link: 'https://www.udemy.com/course/modern-typescript/?couponCode=696E6F58455E238EB221',
     stepik: 'https://stepik.org/a/121859/pay?promo=2419586b3423a7db' + STEPIK_UTM,
     poster: TSImg,
   },
@@ -93,7 +93,7 @@ export const courses = [
     description:
       'Освойте Jest и Testing Library - стандарты на рынке тестовых инструментов. Пишите тесты для своих проектов!',
     tags: ['JavaScript', 'React', 'Jest'],
-    link: 'https://www.udemy.com/course/draft/5177956/?couponCode=3C6CD95E1522465F6B25',
+    link: 'https://www.udemy.com/course/draft/5177956/?couponCode=A8F83C0C0D70C67A97A5',
     poster: TestingImg,
     stepik: 'https://stepik.org/a/200433/pay?promo=e7d6e46070e72c83' + STEPIK_UTM,
   },
@@ -104,7 +104,7 @@ export const courses = [
     description:
       'Освойте множество крутых инструментов в одном курсе! Контроль версий, сборка, терминал, лайфхаки и многое другое.',
     tags: ['NPM', 'Terminal', 'Git'],
-    link: 'https://www.udemy.com/course/webtools/?couponCode=67928FC7D494DB9513D5',
+    link: 'https://www.udemy.com/course/webtools/?couponCode=C05F3C8015A253CB9D07',
     stepik: 'https://stepik.org/a/114174/pay?promo=4b4315421805f30b' + STEPIK_UTM,
     poster: WebToolsImg,
   },
@@ -115,7 +115,7 @@ export const courses = [
     description:
       'Освойте минимально необходимые технологии современной верстки сайтов и начните работать веб-разработчиком в IT сфере!',
     tags: ['HTML', 'CSS', 'JavaScript'],
-    link: 'https://www.udemy.com/course/web-coder/?couponCode=71AEE24CC673276F8589',
+    link: 'https://www.udemy.com/course/web-coder/?couponCode=A2190CE50196CF59D84C',
     stepik: 'https://stepik.org/a/120081/pay?promo=ac333871c4841cc1' + STEPIK_UTM,
     poster: WebCoderImg,
   },
@@ -126,7 +126,7 @@ export const courses = [
     description:
       'Получите необходимые навыки для создания собственной обучающей платформы с готовой системой Moodle.',
     tags: ['LMS', 'CMS', 'Moodle'],
-    link: 'https://www.udemy.com/course/moodle3-admin/?couponCode=164513AF38E1F43AE2D9',
+    link: 'https://www.udemy.com/course/moodle3-admin/?couponCode=125AE5E59C7E2AF66854',
     poster: MoodleImg,
   },
 ];
