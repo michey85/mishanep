4719264c7d9d0a4c5cf9a3be4103b421ad40e917
Mikhail Nepomniashchii <michey85@gmail.com>
2025-06-04 23:34:38 +0200

    remove husky

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,14 +2,14 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   testDir: './playwright',
-  timeout: 44000,
+  timeout: 50000,
   retries: 1,
   reporter: [['list'], ['html', { outputFolder: 'playwright-report' }]],
   use: {
     headless: true,
     baseURL: 'http://localhost:3000',
     screenshot: 'only-on-failure',
-    video: 'retain-on-failure',
+    video: 'off',
     trace: 'on-first-retry',
     viewport: { width: 1280, height: 720 }
   },
@@ -17,10 +17,9 @@ export default defineConfig({
     command: 'npm start',
     port: 3000,
     timeout: 120 * 1000,
-    reuseExistingServer: !process.env.CI, // Reuse in dev, not in CI
+    reuseExistingServer: !process.env.CI,
   },
   projects: [
     { name: 'chromium', use: { browserName: 'chromium' } },
-    { name: 'firefox', use: { browserName: 'firefox' } }
   ]
 });
